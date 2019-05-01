d67b2844d1be72f094818cb869eeefc41fba62e8
Tyler Ellingford <tyler@Rickys-MacBook-Pro.local>
2019-05-01 09:17:53 -0600

    completed app-1

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -2,6 +2,15 @@ import React, { Component } from 'react'
 import './App.css'
 
 class App extends Component {
+
+  constructor(){
+    super();
+
+    this.state = {
+      input: '',
+    }
+  }
+
   handleInput(value) {
     this.setState({
       input: value
@@ -11,11 +20,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <input onChange={(e) => this.handleInput(e)} />
+        <input type='text' onChange={(e) => this.handleInput(e.target.value)} />
         <h1>{this.state.input}</h1>
       </div>
     )
   }
 }
 
-export default App
+export default App;
